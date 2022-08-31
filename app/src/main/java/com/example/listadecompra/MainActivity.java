d75ea7ec7af7f1fde4ff6b4fb5f183466c947fd8
Laboratorio <laboratorio@lab.unigran>
2022-08-30 21:53:44 -0400

    BancoDados

--- app/src/main/java/com/example/listadecompra/MainActivity.java
+++ app/src/main/java/com/example/listadecompra/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.listadecompra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import com.example.bancoDados.DBHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,11 +20,14 @@ public class MainActivity extends AppCompatActivity {
     EditText Nome;
     EditText Marca;
     ListView Lista;
+    SQLiteDatabase conexao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DBHelper db = new DBHelper(this);
+        conexao = db.getWritableDatabase();
 
         Nome = findViewById(R.id.EditText_Produto);
         Marca = findViewById(R.id.EditText_Marca);
